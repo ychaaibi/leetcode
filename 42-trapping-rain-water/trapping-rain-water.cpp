a19f7d202011ad88b90b95f975c1ed2f68d35855
Youness Chaaibi <89740067+ychaaibi@users.noreply.github.com>
2024-04-14 16:08:54 +0100

    Time: 12 ms (40.34%) | Memory: 23.4 MB (6.98%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int ans;
+    
+    int get( int start, int end, int step, vector<int>& height )
+    {
+        stack<int> st;
+        int        mx = -1, next_end=start;
+        
+        for ( int i=start; i - step != end; i += step )
+        {         
+            if ( height[i] < mx )
+            {
+                st.push( i );
+                continue ;
+            }
+            
+            while ( !st.empty() )
+            {
+                ans += mx - height[st.top()];
+                st.pop();
+            }
+            
+            mx = height[i];
+            st.push( i );
+            next_end = i;
+        }
+        return ( next_end );
+    }
+    
+    int trap(vector<int>& height) {
+        int end = get(0, height.size() - 1, 1, height );
+        get( height.size() - 1, end , -1, height );
+        return ( ans );
+    }
+};
