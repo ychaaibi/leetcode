342ec3a305a86c429c7c233845d1acc9ec45406b
Youness Chaaibi <89740067+ychaaibi@users.noreply.github.com>
2024-04-30 10:30:13 +0100

    Time: 360 ms (49.70%) | Memory: 18.6 MB (53.89%) - LeetSync

--- 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     long long wonderfulSubstrings(string word) {
-        unordered_map<bitset<10>, int>  hash;
-        bitset<10>                      bits;
+        unordered_map<int, int>         hash;
+        int                             bits = 0;
         long long                       ans = 0;
 
         hash[bits] = 1;
 
         for ( int i=0; i<word.size(); i++ )
         {
-            bits[word[i] - 'a'] = !bits[word[i] - 'a'];
+            if ( bits & ( 1 << ( word[i] - 'a' ) ) )
+                bits -= ( 1 << ( word[i] - 'a' ) );
+            else
+                bits += ( 1 << ( word[i] - 'a' ) );
+
             hash[bits]++;
 
             ans += hash[bits] - 1;
 
             for ( int b=0; b<10; b++ )
             {
-                bits[b] = !bits[b];
+                int save = bits;
+
+                if ( bits & ( 1 << b ) )
+                    bits -= ( 1 << b );
+                else
+                    bits += ( 1 << b );
 
                 ans += ( ( hash.find(bits) != hash.end() ) ? hash[bits] : 0 );
 
-                bits[b] = !bits[b];
+                bits = save;
             }
         }
 
         return ( ans );
     }
 };
-
-// 145
-
-// 10101
-
-// 10101
-// 10101
-
