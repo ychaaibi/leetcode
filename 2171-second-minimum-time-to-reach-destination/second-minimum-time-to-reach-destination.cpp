1d94caeeacd1664de985b24dc43a335941d62701
Youness Chaaibi <89740067+ychaaibi@users.noreply.github.com>
2024-07-28 10:06:53 +0100

    Time: 562 ms (28.79%) | Memory: 250.5 MB (5.21%) - LeetSync

---
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
@@ -0,0 +1,121 @@
+class Solution {
+public:
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+        vector<vector<int>> graph( n );
+
+        for ( int i=0; i<edges.size(); i++ )
+            graph[edges[i][0] - 1].push_back( edges[i][1] - 1 ), graph[edges[i][1] - 1].push_back( edges[i][0] - 1 );
+
+        queue<pair<int,int>>    pq;
+
+        vector<unordered_set<int>>  parent( n );
+        vector<int>                 path( n, INT_MAX );
+
+        path[0] = 1;
+
+        pq.push( make_pair(0, 1) );
+
+        while ( !pq.empty() )
+        {
+            auto nd_count = pq.front().second, nd = pq.front().first;
+
+            pq.pop();
+
+            for ( auto neigh : graph[nd] )
+            {
+                if ( path[neigh] == INT_MAX )
+                {
+                    pq.push( make_pair(neigh, nd_count + 1) );
+                    path[neigh] = nd_count + 1;
+                }
+                
+                // cout << path[neigh] << " " << nd_count + 1 << endl;
+                if ( nd_count + 1 == path[neigh] )
+                    parent[neigh].insert( nd );
+            }
+        }
+
+        vector<int>    is_in_sp( n, -1 );
+        queue<pair<int,int>>      q;
+        
+        is_in_sp[n - 1] = 1;
+
+        q.push( make_pair( n - 1, 1 ) );
+
+        while ( !q.empty() )
+        {
+            auto curr = q.front().first, nd_c = q.front().second;
+
+            q.pop();
+
+            for ( auto p : parent[curr] )
+            {
+                // cout << "here " << curr << ", " << p << endl;
+                if ( is_in_sp[ p ] != -1 )
+                    continue ;
+
+                is_in_sp[ p ] = nd_c + 1;
+                q.push( make_pair(p, nd_c + 1) );
+            }
+        }
+
+        // for ( int i=0; i<n; i++ )
+        //     cout << is_in_sp[i] << ", ";
+        // cout << endl; 
+
+        vector<bool>    vis( n, false );
+
+        pq.push( make_pair(0, 1) );
+        vis[0] = true;
+
+        int ans = INT_MAX;
+
+        while ( !pq.empty() )
+        {
+            auto nd_count = pq.front().second, nd = pq.front().first;
+
+            pq.pop();
+
+            for ( auto neigh : graph[nd] )
+            {
+                if ( is_in_sp[neigh] != -1 )
+                {
+                    if ( nd_count + is_in_sp[neigh] > is_in_sp[0] )
+                        ans = min(ans, nd_count + is_in_sp[neigh]);
+                }
+
+                if ( vis[neigh] )
+                    continue ;
+
+                vis[neigh] = true;
+
+                pq.push( make_pair(neigh, nd_count + 1) );
+            } 
+        }
+
+        cout << ans << " " << is_in_sp[0] << endl;
+
+        // for ( int i=n - 1; i>=0; i-- )
+        // {
+        //     cout << "node " << i + 1 << endl;
+
+        //     for ( auto p : parent[i] )
+        //         cout << p + 1 << ", ";
+        //     cout << endl;
+        // }
+
+        int _ans = 0;
+
+        while ( --ans )
+        {
+            cout << _ans << " " << change << " " << ( _ans / change ) << endl;
+
+            if ( ( _ans / change ) % 2  )
+                _ans = _ans + ( change - (_ans % change) );
+            cout << "aftr " << _ans << endl;
+            _ans += time;
+        }
+
+        return ( _ans );
+    }
+};
